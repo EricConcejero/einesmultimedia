623b4ebdc0094f69797b89af039336158b9ddc76
marcplanse4 <mplanse4@gmail.com>
2025-01-29 16:49:16 +0100

    sonidos y img

--- script.js
+++ script.js
@@ -25,4 +25,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
             event.dataTransfer.setData('text', event.target.id);
         });
     });
-});
+
+    document.body.addEventListener('click', (event) => {
+        if (event.target.matches('#botoSorollCotxe1, #botoSorollCotxe2, #botoSorollCotxe3, #botoSorollCotxe4')) {
+            playSound(event.target.dataset.sound);
+        } else if (event.target.matches('#botoLlegirCotxe1, #botoLlegirCotxe2, #botoLlegirCotxe3, #botoLlegirCotxe4')) {
+            readDescription(event.target.dataset.description);
+        }
+    });
+
+    function playSound(soundFile) {
+        var audio = new Audio(soundFile);
+        audio.play();
+    }
+
+    function readDescription(description) {
+        var msg = new SpeechSynthesisUtterance(description);
+        window.speechSynthesis.speak(msg);
+    }
+
+    });
